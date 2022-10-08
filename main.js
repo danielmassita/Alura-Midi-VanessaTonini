46dfc6cd1f4bc301e766f84864d04ab38a3d19be
danielmassita <daniel.massita@gmail.com>
2022-10-08 00:18:16 -0300

    Aula-03-01 Lista de Elementos

--- main.js
+++ main.js
@@ -6,6 +6,6 @@ function tocaSomClap () {
     document.querySelector('#som_tecla_clap').play();
 }
 
-document.querySelector('.tecla_pom');
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
 // document.querySelector('#som_tecla_pom').play(); //essa linha vai dar erro no console do arquivo ao abrir, pois existe uma ordem de ação... No index.html, o <script> roda primeiro de carregar os sons do <body>, então quando dependemos de elementos dentro do body do html, devemos colocar o script ao final do texto, por exemplo, uma linha acima antes de fechar o </body>
