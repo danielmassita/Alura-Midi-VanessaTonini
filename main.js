2385e85544d5732cba8086c94d08874fe2ffe060
danielmassita <daniel.massita@gmail.com>
2022-10-08 00:14:42 -0300

    Aula-02-03 O que é uma função...

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
-alert('Olá, Mundo! ;)');
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+function tocaSomClap () {
+    document.querySelector('#som_tecla_clap').play();
+}
+
 document.querySelector('.tecla_pom');
+
+// document.querySelector('#som_tecla_pom').play(); //essa linha vai dar erro no console do arquivo ao abrir, pois existe uma ordem de ação... No index.html, o <script> roda primeiro de carregar os sons do <body>, então quando dependemos de elementos dentro do body do html, devemos colocar o script ao final do texto, por exemplo, uma linha acima antes de fechar o </body>
