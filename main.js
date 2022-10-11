dff594668b82d5b989086e606d0e8a2058888fc6
danielmassita <daniel.massita@gmail.com>
2022-10-11 01:45:32 -0300

    Lista Manual [0:length] falta FUNCTION manual

--- main.js
+++ main.js
@@ -3,7 +3,17 @@ function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelectorAll('.tecla')
+const listaDeTeclas = document.querySelectorAll('.tecla'); // abre um NodeList usando a class '.tecla' em comum...
+
+listaDeTeclas[0].onclick = tocaSomPom;
+listaDeTeclas[1].onclick = tocaSomClap;
+listaDeTeclas[2].onclick = tocaSomTim;
+listaDeTeclas[3].onclick = tocaSomPuff;
+listaDeTeclas[4].onclick = tocaSomSplash;
+listaDeTeclas[5].onclick = tocaSomToim;
+listaDeTeclas[6].onclick = tocaSomPsh;
+listaDeTeclas[7].onclick = tocaSomTic;
+listaDeTeclas[8].onclick = tocaSomTom;
 
 //document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
